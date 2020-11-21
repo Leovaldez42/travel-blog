1d146bbeb1873edd46d68445c49f925eeb972c32
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-11-21 23:54:03 +0530

    changed the style of the map

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import * as React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 const App = () => {
 	const [viewport, setViewport] = useState({
 		width: '100vw',
 		height: '100vw',
-		latitude: 37.7577,
-		longitude: -122.4376,
-		zoom: 8
+		latitude: 25.133699,
+		longitude: 82.564430,
+		zoom: 3
 	});
+
+	useEffect(() => {
+		
+	}, []);
 
 	return (
     	<ReactMapGL
 			{...viewport}
+			mapStyle="mapbox://styles/leovaldez42/ckhs0lhv210ba19rmhpbbd25a"
       		mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_TOKEN}
       		onViewportChange={nextViewport => setViewport(nextViewport)}
     	/>
