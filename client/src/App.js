8e50ddc74935f2330698ff558ea77683262d91f8
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-11-23 11:21:40 +0530

    added markup points on all the places

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 
 import { listLogEntries } from './Api';
 
 const App = () => {
+	const [logEntries, setLogEntries] = useState([]);
+	const [showPopup, setShowPopup] = useState({});
 	const [viewport, setViewport] = useState({
 		width: '100vw',
 		height: '100vw',
@@ -15,17 +17,63 @@ const App = () => {
 	useEffect(() => {
 		(async () => {
 			const logEntries = await listLogEntries();
-			console.log(logEntries);
+			setLogEntries(logEntries);
 		})();
 	}, []);
 
 	return (
     	<ReactMapGL
 			{...viewport}
-			mapStyle="mapbox://styles/leovaldez42/ckhs0lhv210ba19rmhpbbd25a"
+			mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       		mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_TOKEN}
-      		onViewportChange={nextViewport => setViewport(nextViewport)}
-    	/>
+      		onViewportChange={nextViewport => setViewport(nextViewport)}>
+			{
+				logEntries.map(entry => (
+					<>
+						<Marker 
+							key={entry._id}
+							latitude={entry.latitude} 
+							longitude={entry.longitude}>
+								
+						<div 							
+							onClick={() => setShowPopup({
+							showPopup,
+							[entry._id] : true,
+							})}
+						>
+							<img 
+								className="marker"
+								style={{
+									height: `${6 * viewport.zoom}px`,
+									width: `${6 * viewport.zoom}px`,
+								}}
+								src="https://i.imgur.com/y0G5YTX.png" 
+								alt="marker" />
+						</div>
+
+						</Marker>
+
+						{
+							showPopup[entry._id] ? (
+								<Popup
+									latitude={entry.latitude} 
+									longitude={entry.longitude}
+									closeButton={true}
+									closeOnClick={false}
+									onClose={() => this.setState({showPopup: false})}
+									anchor="top" >
+									<div>
+										<h3> { entry.title } </h3>
+										<p> { entry.comments } </p>
+									</div>
+								</Popup>
+							) : null
+						}
+					</>
+				))
+			}
+
+		</ReactMapGL>
   	);
 }
 
